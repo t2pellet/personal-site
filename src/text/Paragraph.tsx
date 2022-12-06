40141053ab4f1b8586c0f70cd905c00b35566310
Tenzin Pelletier <pelletiertenzin2000@gmail.com>
2022-12-06 13:42:13 +0100

    implement ProjectCard component

--- src/text/Paragraph.tsx
+++ src/text/Paragraph.tsx
@@ -1,11 +1,25 @@
 import React, { PropsWithChildren } from 'react';
 import { Typography } from '@mui/material';
 
-export default function Paragraph(props: PropsWithChildren) {
-    const { children } = props;
+type ParagraphProps = PropsWithChildren & {
+    colour?: string;
+    size?: string;
+};
+
+const defaultProps: ParagraphProps = {
+    colour: 'text.secondary',
+    size: '16px'
+};
+
+function Paragraph(props: ParagraphProps) {
+    const { children, colour = defaultProps.colour, size = defaultProps.size } = props;
     return (
-        <Typography variant='body1' paragraph color='text.secondary'>
+        <Typography variant='body1' paragraph color={colour} fontSize={size}>
             {children}
         </Typography>
     );
 }
+
+Paragraph.defaultProps = defaultProps;
+
+export default Paragraph;
