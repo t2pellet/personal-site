e7fbaf54e92a0acab83474b934d13a68f17372b1
t2pellet <t2pellet@uwaterloo.ca>
2023-11-18 23:19:53 -0500

    feat: add GA tracking
    Signed-off-by: t2pellet <t2pellet@uwaterloo.ca>

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -31,7 +31,19 @@ export default function RootLayout({
       data-theme='dracula'
       className='w-full overflow-x-hidden'
     >
-      <Script src='js/hashchange.js' />
+      <Script id='hash-change' src='js/hashchange.js' />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script id='google-analytics'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
       <body>
         <Particles />
         <NavBar />
