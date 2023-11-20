866d4f4aaadb6b356098b0d73e0db2723503d159
t2pellet <t2pellet@uwaterloo.ca>
2023-11-19 19:20:28 -0500

    fix: revalidate hook

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,10 +1,11 @@
 import { revalidateTag } from 'next/cache';
+import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   const token = request.headers.get('Authorization')?.substring(7);
   if (token === process.env.STRAPI_HOOK_TOKEN) {
     revalidateTag('strapi');
-    return Response.json({ revalidated: true });
+    return Response.json({ revalidated: true }, { status: 200 });
   }
-  return Response.error();
+  return new NextResponse('Invalid strapi token', { status: 403 });
 }
